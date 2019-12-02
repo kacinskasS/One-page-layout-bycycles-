eb235118d67e284f67f25c5ad368f0ab0f6cd437
kacinskasS <53061402+kacinskasS@users.noreply.github.com>
2019-12-02 14:21:52 +0200

    Add files via upload

---
+++ Bicycles/index.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Bycycle photoshop layout</title>
+        <link rel="stylesheet" type="text/css" href="reset.css">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <meta charset="utf-8">
+        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <header>
+            <nav>
+                <ul>
+                    <div class="sandwichdiv"><img src="images/menu%20(2).svg" class="sandwich"></div>
+                    <li>About us</li>
+                    <li>.</li>
+                    <li>Work</li>
+                    <li><img src="images/Logo.png"></li>
+                    <li>Shop</li>
+                    <li>.</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+        <section class="sectionfirst">
+            <div>
+                <img src="images/Icon.png">
+                <h1>A NEW GENERATION OF VINTAGE BIKE</h1>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+                </p>
+            </div>
+        </section>
+        <section class="sectionsecond">
+            <div class="leftside">
+                <img src="images/Logobike.png" class="bike">
+            </div>
+            <div class="rightside">
+                <div class="topright">
+                    <img src="images/icon2.png">
+                    <h2>vintage Oliva</h2>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived.
+                    </p>
+                    <hr>
+                    <div class="sqoure"></div>
+                </div>
+                <img src="images/Layer%2011.jpg" class="bottomright">
+            </div>
+        </section>
+        <section class="sectionthird">
+            <div class="leftside">
+                <div class="topleft">
+                    <img src="images/Icon3.png">
+                    <h2>La Boriosa</h2>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived.
+                    </p>
+                    <hr>
+                    <div class="sqoure"></div>
+                </div>
+                <img src="images/Layer%2013.jpg" class="bottomleft">
+            </div>
+            <div class="rightside">
+                <img src="images/Logo4.png" class="vintage">
+            </div>
+        </section>
+        <section class="sectionfourfth">
+            <div class="leftside">
+                <img src="images/Logo15.png" class="logobike">
+            </div>
+             <div class="middleside">
+                <div class="topmiddle">
+                    <img src="images/Iconmiddle.png">
+                    <h2>retrò Bike - M. Hulot</h2>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived.
+                    </p>
+                    <hr>
+                    <div class="sqoure"></div>
+                </div>
+                <img src="images/Layer%2016.jpg" class="bottommiddle">
+            </div>
+            <div class="rightside"></div>
+        </section>
+        <section class="sectionfifth">
+            <div class="inner">
+                <img src="images/Icontw.png">
+                <h3>AOD New York  @aod <span>/  35 min</span></h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut tellus ac nulla semper rhoncus. Nullam a odio porttitor, dictum turpis vitae, pretium ante amet.
+                </p>
+                <ul class="dots">
+                    <li><div class="active"></div></li>
+                    <li><div></div></li>
+                    <li><div></div></li>
+                </ul>
+            </div>
+        </section>
+        <section class="sectionsixth">
+            <div class="leftside">
+                <div class="top">
+                    <div class="left">
+                        <div class="overlay">
+                            <div class="icontext">
+                                <img src="images/Shapebikelogo.png">
+                                <h4>shop now</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="right">
+                        <div class="overlay">
+                            <div class="icontext">
+                                <img src="images/Shapebikelogo.png">
+                                <h4>shop now</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="bottom">
+                    <div class="overlay">
+                            <div class="icontext">
+                                <img src="images/Shapebikelogo.png">
+                                <h4>shop now</h4>
+                            </div>
+                    </div>
+                </div>
+            </div>
+            <div class="middle">
+                <div class="left">
+                    <div class="overlay">
+                        <div class="icontext">
+                            <img src="images/Shapebikelogo.png">
+                            <h4>shop now</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="right">
+                    <div class="overlay">
+                            <div class="icontext">
+                                <img src="images/Shapebikelogo.png">
+                                <h4>shop now</h4>
+                            </div>
+                    </div>
+                </div>
+            </div>
+            <div class="rightside">
+                <div class="top">
+                    <div class="overlay">
+                            <div class="icontext">
+                                <img src="images/Shapebikelogo.png">
+                                <h4>shop now</h4>
+                            </div>
+                        </div>
+                </div>
+                <div class="bottom">
+                    <div class="left">
+                        <div class="overlay">
+                            <div class="icontext">
+                                <img src="images/Shapebikelogo.png">
+                                <h4>shop now</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="right">
+                        <div class="overlay">
+                            <div class="icontext">
+                                <img src="images/Shapebikelogo.png">
+                                <h4>shop now</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="sectionseventh">
+            <div>
+                <button>DISCOVER THE SHOP</button>
+            </div>
+        </section>
+        <section class="sectioneighth">
+            <img class="logo" src="images/Logo.png">
+            <h3>Stay on the saddle!</h3>
+            <div class="inputandbutton">
+                <input placeholder="enter your email...">
+                <button>go</button>
+            </div>
+        </section>
+        <footer>
+            <div class="inner">
+                <div class="leftside">
+                    <h2>AOD New York</h2>
+                    <h3>2250 Lexington Avenue</h3>
+                    <h4>New York, NY 10035  - info@domain.com</h4>
+                </div>
+                <div class="middle">
+                    <img src="images/twiter.png">
+                    <img src="images/facebook.png" class="fb">
+                    <img src="images/p.png">
+                </div>
+                <div class="rightside">
+                    <h2>Handcrafted by</h2>
+                    <img src="images/Y%20E%20B%20O%20Logo.png">
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
